2d323b7b63c7572d35dcfde79a7120ac98b91563
mintslime1288 <mintslime1288@gmail.com>
2026-07-14 21:22:26 +0900

    Include PDF renderer files in Vercel function

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
     "pdfjs-dist",
     "@napi-rs/canvas"
   ],
+  outputFileTracingIncludes: {
+    "/api/jobs/[tool]": [
+      "./node_modules/pdf-to-img/**/*",
+      "./node_modules/pdfjs-dist/**/*",
+      "./node_modules/@napi-rs/canvas/**/*",
+      "./node_modules/@napi-rs/canvas-linux-x64-gnu/**/*"
+    ]
+  }
 };
 
 export default nextConfig;
