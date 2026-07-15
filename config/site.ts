58bc916bc33cb9b24a93737c6c01df5e2267b0ec
mintslime1288 <mintslime1288@gmail.com>
2026-07-16 01:48:06 +0900

    update contact email

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = "PDF Toolkit";
 export const CONTACT_EMAIL =
-  process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? "mintslime1288@gmail.com";
+  process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? "jey.dev1288@gmail.com";
 
 export const MAX_FILE_BYTES = 20 * 1024 * 1024;
 export const MAX_TOTAL_BYTES = 50 * 1024 * 1024;
